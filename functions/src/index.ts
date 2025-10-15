a1f0911f33dff0d567c1421f1bafdd364de86671
andrew.t.mouser <andrew.t.mouser@gmail.com>
2025-10-15 07:32:45 +0000

    feat: Create a clean and valid Firebase project

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,5 @@
-
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
 
-admin.initializeApp();
-
-// A simple v1 HTTP function
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
