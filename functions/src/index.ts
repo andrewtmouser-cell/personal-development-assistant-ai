eab56c1e723152588b9fa50e4166f11b3e1ff491
andrew.t.mouser <andrew.t.mouser@gmail.com>
2025-10-15 07:07:16 +0000

    fix: Remove service account key from code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,18 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
 
 // Initialize Firebase Admin SDK
-import * as serviceAccount from "./personal-development-ass-6e364-9d18a0128b1f.json";
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccountInfo)
-});
+admin.initializeApp();
 
 
 // Start writing functions
